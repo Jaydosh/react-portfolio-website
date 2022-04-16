4340958bd2eb963df3e9c8fb56ba42c8fe92916a
Jaydosh <jayden.gaydosh@gmail.com>
2022-04-16 17:31:59 -0400

    creating skelital structure

---
+++ react-portfolio-website-1/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Experence from './components/Experience/Experience'
+import Contact from './components/Contact/Contact'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experence />
+      <Contact />
+    </>
+  )
+}
+
+export default App
