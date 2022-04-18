28923cd38eabdf8847c8b2c51736f7e29bb1ef46
Jaydosh <jayden.gaydosh@gmail.com>
2022-04-17 21:03:58 -0400

    Adding navigation bar

--- react-portfolio-website-1/src/components/Nav/Nav.jsx
+++ react-portfolio-website-1/src/components/Nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {SiAboutdotme} from 'react-icons/si'
+import {BsJournalBookmark} from 'react-icons/bs'
+import {RiContactsLine} from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><SiAboutdotme /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience') } className={activeNav === '#experience' ? 'active' : ''}><BsJournalBookmark /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiContactsLine /></a>
+    </nav>
   )
 }
 
