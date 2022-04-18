cc178dfe55fb315da21862f50745a4dcabb94d5c
Jaydosh <jayden.gaydosh@gmail.com>
2022-04-17 23:42:22 -0400

    Adding portrate to header

--- react-portfolio-website-1/src/components/Header/Header.jsx
+++ react-portfolio-website-1/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/Portrait.jpg'
 
 const Header = () => {
   return (
@@ -10,6 +11,9 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Jayden Gaydosh</h1>
         <h5 className="text-light">Mechaincal Engineer</h5>
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
         <CTA />
         <HeaderSocials />
       </div>
